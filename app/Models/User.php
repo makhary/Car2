0324a773a47ee3e74e946363f4a131bd55debb67
makhary <48020838+makhary@users.noreply.github.com>
2025-11-29 23:20:38 +0000

    Add Filament v4 admin panel scaffolding

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasApiTokens, HasFactory, Notifiable;
@@ -70,5 +72,12 @@ class User extends Authenticatable
     public function maintenances()
     {
         return $this->hasMany(Maintenance::class, 'reported_by');
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->roles()
+            ->whereIn('name', ['admin', 'fleet_manager'])
+            ->exists();
     }
 }
